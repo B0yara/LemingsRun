d8048284832030b18bb2f07eab06b1e309157523
Grigorii Boyarkin <grigorii_boyarkin@mail.ru>
2021-03-01 20:34:09 +0300

    сделал перемещение юнитов

--- Assets/Scripts/UnitBehaviour.cs
+++ Assets/Scripts/UnitBehaviour.cs
@@ -6,34 +6,46 @@ public class UnitBehaviour : MonoBehaviour
 {
     Rigidbody rigidBody;
     [SerializeField]
-    [Range(0,0.1f)]
+    [Range(0,10f)]
     float speed;
+    float gravity=  -9.8f;
     public bool isRunning = false;
    public Animator anim;
-  
-  
+  CharacterController controller;
+    Vector3 direction = Vector3.zero;
+
     void Start()
     {
-        rigidBody = GetComponent<Rigidbody>();
+
+        
         anim = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
-        if (isRunning)
+        
+        if(!controller.isGrounded)
         {
-          
-             rigidBody.AddRelativeForce( Vector3.forward * speed, ForceMode.VelocityChange);  
+            
+            direction.y =  gravity;
         }
-
+        else
+        {
+            if (isRunning)
+            {
+                direction = transform.TransformDirection(Vector3.forward * speed);
+            }
+            direction.y = 0;
+        }
          anim.SetBool("isRuning", isRunning);
         if(Input.GetKeyUp(KeyCode.F))
         {
             anim.SetTrigger("isDancing");
         }
-        
-       
+
+        controller.Move(direction * Time.deltaTime);
     }
 }
